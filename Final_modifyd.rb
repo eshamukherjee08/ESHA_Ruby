19d73e29964a8f297b05eac1a8c12207bd3c37b0
Shreya Bhatia <shreya@Shreya-Bhatias-MacBook-Pro.local>
2011-10-14 10:18:02 +0530

    a lil clean-up

--- Final_modifyd.rb
+++ Final_modifyd.rb
@@ -1,11 +1,14 @@
 class Dictionary
+
   def initialize
-    @@option_list = Array.new              #a global array to store the option's list read from words.txt
-    @@avail_list = Array.new               #global array to store match for each individual option wrt to wrong word.
+    @@option_list = Array.new              # to store the option's list read from words.txt
+    @@avail_list = Array.new               # to store match for each individual option wrt to wrong word.
   end
+
   def check(wrong_word)
     read_file(wrong_word)
   end
+
   def read_file(wrong_word)                         #function to read word repository file.
     array_for_words = @@option_list
     str = ""
@@ -17,6 +20,8 @@ class Dictionary
     end
     process(wrong_word)                                   #calling function process at line no. 19
   end
+  
+  
   def process(wrong_word)
     array = @@option_list
     puts "The Wrong word is : " + wrong_word
@@ -25,14 +30,16 @@ class Dictionary
     end
     compare                                    #calling function compare (line no:62) to find largest match(s) and display
   end
-  def lcs(a, b)                                #function to find largest match of substring for each option wrt wrong word.
-      result=""                                #stores resulted sub-string
-      arr=Array.new(a.size){Array.new(b.size)} #2D array to match occurance.
-      len=0                                    #to store largest num in array that denotes longest match.
-      ls_str=0                                 #to store the last sub-string.
+  
+  
+  def lcs(a, b)                                   #function to find largest match of substring for each option wrt wrong word.
+      result = ""                                 #stores resulted sub-string
+      arr = Array.new(a.size){Array.new(b.size)}  #2D array to match occurance.
+      len=0                                       #to store largest num in array that denotes longest match.
+      ls_str=0                                    #to store the last sub-string.
       a.scan(/./).each_with_index{|l1,row_indx|
         b.scan(/./).each_with_index{|l2,col_indx|
-          if l1==l2                            #if-main start, comapring each letter
+          if l1==l2                               #if-main start, comapring each letter
             if (row_indx==0 || col_indx==0)                  #if-1 both index 0 then store 1 initially
               arr[row_indx][col_indx]=1
             else
@@ -56,17 +63,15 @@ class Dictionary
       }
       @@avail_list << result                   #filling array with all matches
   end
+  
   def compare                                 #function to find longest match(s)
    fk, mat = @@avail_list, @@option_list
-   len = Array.new
+   len = w_match = []
    fk.each{|x| len << x.length}
    enum_fk = len.to_enum
    maximum = enum_fk.max_by{|x| x}
-   w_match = Array.new
    for i in 0...fk.size
-     if(maximum == fk[i].length)
-       w_match << mat[i]
-     end
+     w_match << mat[i] if(maximum == fk[i].length)
    end
    puts "The match(s) for your word is :"
    w_match.each{|x| puts x}
