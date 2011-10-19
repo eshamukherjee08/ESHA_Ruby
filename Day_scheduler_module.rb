3dc61c818d0cbe03059f92d6387d8caa683e2714
Shreya Bhatia <shreya@Shreya-Bhatias-MacBook-Pro.local>
2011-10-19 12:12:54 +0530

    Initial Review

--- Day_scheduler_module.rb
+++ Day_scheduler_module.rb
@@ -1,11 +1,11 @@
+### GENERAL COMMENTS - In case of holidays schedule meeting the next working day
+#### Use Time/Date/DateTime for time manipulations and calculations throughout 
 =begin
 Class Variables Used
 @@date_hash : to store updated dates
 @@day_hash : to store updated days.
 @@close_day : to store list of closed days.
 @@close_date : to store list of closed dates.
-@@start_time : to store default start time.
-@@end_time : to store default end time.
 
 Variables Used
 num : for converting digit to week day.
@@ -32,8 +32,9 @@ time1/time2 : used for subtracting time values.
 require 'date'
 module Usable
   def parser(date)                                            #function to return day of week(0-6).
+    ## COMMENT - Should be written as - DateTime.strptime(date, "%b %d, %Y").wday directly, no need for variable
     cur_day = DateTime.strptime(date, "%b %d, %Y").wday       #conversion to corresponding number.
-    return cur_day
+    cur_day
   end
   
   def num_converter(num)                                      #function to convert in day from number generated.
@@ -57,41 +58,51 @@ module Usable
     end
   end
   
+  ## COMMENT - Use Time/Date/DateTime for time manipulations and calculations
   def time_diff(time1,time2)                              #function to calculate difference in time.
-    if(time1.to_i>time2.to_i)
-      m = ((time1/100)-(time2/100))*60-(time2%100)
+    if(time1.to_i > time2.to_i)
+      m = ((time1/100) - (time2/100))*60 - (time2%100)
     else
-      m = ((time2/100)-(time1/100))*60-(time1%100)
+      m = ((time2/100) - (time1/100))*60 - (time1%100)
     end
     return m
   end
 end
 class BusinessCenterHours 
   include Usable
-   @@date_hash,@@day_hash,@@close_day,@@close_date = Hash.new,Hash.new,Array.new,Array.new
+ 
+   @@date_hash, @@day_hash, @@close_day, @@close_date = Hash.new,Hash.new,Array.new,Array.new
    @@close_day << "sun"
+   
   def initialize(start_time,end_time)
     @start_time, @end_time = start_time, end_time
   end
   
   def update(date_day,start_t,end_t)                           #Function to update timings on basis of day and date.
+    # COMMENT  - Should be written as - date_day.is_a?(Symbol)
     if(/^:([a-zA-Z]{3})$/ =~ date_day)                         #check if value entered is day?
-      @@day_hash[$1]=[start_t,end_t]
+      @@day_hash[$1] = [start_t,end_t]
     else
       @@date_hash[date_day] = [start_t,end_t]
     end
   end
   
   def closed(c_day)                                           #to maintain list of holidays.
-    if(/^([a-zA-Z]{3})$/ =~ c_day)
+    # COMMENT  - Should be written as - date_day.is_a?(Symbol)
+    # (/^([a-zA-Z]{3})$/ =~ c_day) ? 
+    ### Can be written as -
+    # (/^([a-zA-Z]{3})$/ =~ c_day) ? (@@close_day << c_day) : (@@close_date << c_day)
+    
+    if(/^([a-zA-Z]{3})$/ =~ c_day) ? (@@close_day << c_day) : (@@close_date << c_day)
       @@close_day << c_day                                    #if day entered as closed.
     else
       @@close_date << c_day                                   #if date entered as closed
     end
   end
  
-  def calculate_deadline(duration,app_date,app_time)            #function to check whether date is current or closed if yes terminates else calls schedular function.
+  def calculate_deadline(duration, app_date, app_time)            #function to check whether date is current or closed if yes terminates else calls schedular function.
     in_date = num_converter(parser(app_date))
+    
     if(Date.parse(app_date) <= Date.today)
       puts "Sorry..Cannot schedule for today or past date!today is :#{Date.today}"
     else
@@ -100,13 +111,14 @@ class BusinessCenterHours
       elsif(@@close_day.include?(in_date))
         print "Sorry!day Closed/Off."
       else
-        scheduler(duration,app_date,app_time)
+        scheduler(duration, app_date, app_time)
       end
     end
   end 
   
-  def scheduler(duration,app_date,app_time)                                                                     #function to schedule meeting and provide deadline.
+  def scheduler(duration, app_date, app_time)                                                                     #function to schedule meeting and provide deadline.
     duration_req, app_time, in_date = ((duration.to_i)*60), (app_time.to_i), (num_converter(parser(app_date)))  
+    
     if(@@date_hash.include?(app_date))
       val = @@date_hash.values_at(app_date)
       starting, ending = (val[0][0].to_i), (val[0][1].to_i)
@@ -116,23 +128,26 @@ class BusinessCenterHours
     else
       starting, ending = (@start_time.to_i), (@end_time.to_i)
     end
-     if(app_time>ending)
-      f, app_date = 0, (Date.parse(app_date)+1).strftime("%b %d,%Y")
-      while(f == 0) do
-        in_date = num_converter(parser(app_date))
-        if(@@close_day.include?(in_date) || @@close_date.include?(app_date))
-          app_date = (Date.parse(app_date)+1).strftime("%b %d,%Y")
-        else
-          app_time, ending, f = get_start(app_date), get_end(app_date), 1
-        end
-      end
+    
+    if(app_time > ending)
+     f, app_date = 0, (Date.parse(app_date)+1).strftime("%b %d,%Y")
+     while(f == 0) do
+       in_date = num_converter(parser(app_date))
+       if(@@close_day.include?(in_date) || @@close_date.include?(app_date))
+         app_date = (Date.parse(app_date)+1).strftime("%b %d,%Y")
+       else
+         app_time, ending, f = get_start(app_date), get_end(app_date), 1
+       end
+     end
     end
+    
+    
     time_avail = time_diff(ending,app_time)
     if duration_req < time_avail
       if duration_req <60
         target_time = app_time+duration_req
       else
-        target_time = app_time+((duration_req/60)*100)+(duration_req%60)
+        target_time = app_time+((duration_req/60)*100)+(duration_req % 60)
       end
       in_date = num_converter(parser(app_date))
       puts "Deadline: #{in_date} #{app_date}, #{target_time}" 
@@ -205,7 +220,9 @@ class BusinessCenterHours
     return ending
   end
 end
-h=BusinessCenterHours.new('0900','1500')
+
+h = BusinessCenterHours.new('0900','1500')
+
 h.update(":sat","1000","1700")
 h.update("Jan 4,2011","0800","1300")
 h.closed("thu")
