4e57b1748e49ca0da2eb9bbfdd481e4fc14d476f
Esha Mukherjee <eshadgr8@gmail.com>
2011-11-02 16:03:50 +0530

    Added adv ruby exercises

---
+++ adv_ruby/EX_interactive_program.rb
@@ -0,0 +1,11 @@
+main_code = []
+read_line = gets.chomp
+while !(read_line.empty?) do
+  if(read_line == "q" || read_line == "Q")
+    abort
+  else
+    main_code << read_line
+    read_line = gets.chomp
+  end
+end
+eval "#{main_code.join(";")}"
