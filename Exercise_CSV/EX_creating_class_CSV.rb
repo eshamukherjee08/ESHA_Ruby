a293e89f8d8d1fe33c2fa09234c4f5fa84a705df
Esha Mukherjee <eshadgr8@gmail.com>
2011-11-07 13:53:06 +0530

    CSV corrections made

--- Exercise_CSV/EX_creating_class_CSV.rb
+++ Exercise_CSV/EX_creating_class_CSV.rb
@@ -8,14 +8,13 @@ module Functioning
   
   # Method to read file and create objects dynamically.
   def reading_file
-    file_name, counter, array_object, array_data, array_function = "#{Class_var}.csv", 0, [], [], []
+    file_name, counter, array_object, array_function = "#{Class_var}.csv", 0, [], []
     File.open(file_name, "r") do |infile|
       while (line = infile.gets)
         if(counter == 0)
-          array_function, counter = line.chomp.split(/,/), 1 
+          array_function, counter = line.chomp.split(/,/), 1
         else
-          array_object << self.class.new("#{line}".chomp)
-          array_data << line
+          array_object << self.class.new("#{line}".chomp, array_function)
         end
       end
     end                                           
@@ -28,7 +27,7 @@ module Functioning
     array_function.each_with_index do |element, index|
       Class_var.class_eval do  
         define_method("#{element}") do
-          puts @variable.split(/,/)[index]
+          puts instance_variable_get("@#{element}")
         end
       end  
     end
@@ -36,22 +35,31 @@ module Functioning
 
 end
 
+
 # Creating class with file name
 Class_var = Object.const_set(file_read, Class.new{
-  include Functioning
+  include Functioning 
   def initialize (*args)
-    instance_variable_set("@variable",args[0])
-  end 
+    if !(args.empty?)
+      for i in 0..args.length 
+        self.class.send(:attr_accessor, args[1][i])
+        instance_variable_set("@#{args[1][i]}",args[0].split(/,/)[i]) 
+      end
+    end
+  end     
+  
+  # def self.fill_data
+  #    
+  #  end
+  #p @array_function 
 })
    
 
 
 arr = Class_var.new.reading_file
-p arr
-# arr[0].name
-# arr[0].age
-# arr[0].city
-# arr[1].name
-# arr[1].age
-# arr[1].city
-        
+arr[0].name
+arr[0].age
+arr[0].city
+arr[1].name
+arr[1].age
+arr[1].city           
