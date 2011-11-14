b0f9ed366293bf36f366f6c0ee26c6270605d808
Esha Mukherjee <eshadgr8@gmail.com>
2011-11-14 13:02:32 +0530

    Commiting to pull

--- adv_ruby/Esha_Test_Bonus_inheritance.rb
+++ adv_ruby/Esha_Test_Bonus_inheritance.rb
@@ -1,30 +1,30 @@
 class Class
   def carryover_attr(*args)
     args.each do |element|
-      self.instance_eval %{
-        def #{element}
+      self.instance_eval do
+        define_singleton_method (element) do
           if(instance_variable_get("@#{element}"))
             instance_variable_get("@#{element}")
           else
             if(self.superclass.to_s != "Object")
-              self.superclass.send(#{element})
+              self.superclass.send(element)
             end
           end
         end
 
-        def #{element}= val
+        define_singleton_method("#{element}=") do |val|
           if val.class == Array
             if(instance_variable_set("@#{element}",val))
               instance_variable_set("@#{element}",val)
             else
               if(self.superclass.to_s != "Object")
-                self.superclass.send(#{element})
+                self.superclass.send(element)
               end
             end
           end
           instance_variable_set("@#{element}",val)
         end
-      }
+      end
     end
   end
 end
@@ -39,19 +39,23 @@ class B < A
 end 
 
 A.b = 1988 
-#p A.b 
-#p B.b
+p A.b 
+p B.b
+puts "------------------"
 B.b = "hi"
 p B.b
 p A.b
+puts "++++++++++++++++++"
 A.b = 123
-#p A.b
-#p B.b
+p A.b
+p B.b
 puts "***************"
 A.a = [1,2,3,4]
 p A.a
+p B.a
+puts "-----------------"
 B.a = [8]
-#p A.a
+p A.a
 p B.a
 puts "________________"
 B.a << 5
